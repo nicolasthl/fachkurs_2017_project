d23a66c3c3304bb0739eaaeee93b7d332531c160
jannisuhlendorf <jannis.uhlendorf@hu-berlin.de>
2017-05-08 16:10:05 +0200

    copied Jespers molecule to molecules.py and removed obsolete molecule files

--- molecules.py
+++ molecules.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from copy import copy
 
 
@@ -6,64 +5,55 @@ class Molecule:
     def __init__(self, name):
         self.name = name
 
+    def __eq__(self, other):
+        return isinstance(other, type(self)) and self.name == other.name
 
-class Polymer:
-    def __init__(self, valid_monomers, monomers):
+
+class Polymer(Molecule):
+    def __init__(self, name, sequence, valid_monomers):
+        super().__init__(name)
         self.valid_monomers = valid_monomers
-        self.monomers = ''
-        for monomer in monomers:
+        self.sequence = ''
+        self.bindings = []
+        for monomer in sequence:
             self.add_monomer(monomer)
 
+    def __eq__(self, other):
+        return super().__eq__(other) and self.sequence == other.sequence
+
     def __len__(self):
-        return len(self.monomers)
+        return len(self.sequence)
 
     def add_monomer(self, monomer):
         if monomer not in self.valid_monomers:
             raise ValueError('Invalid monomer {}'.format(monomer))
-        self.monomers += monomer
+        self.sequence += monomer
+        self.bindings.append(None)
 
     def calc_mass(self):
-        return sum(self.valid_monomers[monomer] for monomer in self.monomers)
+        return sum(self.valid_monomers[monomer] for monomer in self.sequence)
 
 
 class Ribo(Molecule):
-    def __eq__(self, other):
-        if not isinstance(other, Ribo):
-            return NotImplemented
-        else:
-            return self.name == other.name
+    pass
 
 
-class Protein(Molecule, Polymer):
+class Protein(Polymer):
     amino_acids = {
         'A': 89.0929, 'R': 175.208, 'N': 132.118, 'D': 132.094, 'C': 121.158, 'Q': 146.144, 'E': 146.121,
         'G': 75.0664, 'H': 155.154, 'I': 131.172, 'L': 131.172, 'K': 147.195, 'M': 149.211, 'F': 165.189,
         'P': 115.13,  'S': 105.092, 'T': 119.119, 'W': 204.225, 'Y': 181.188, 'V': 117.146
     }
 
-    def __init__(self, name, monomers=''):
-        Molecule.__init__(self, name)
-        Polymer.__init__(self, self.amino_acids, monomers)
-
-    def __eq__(self, other):
-        if not isinstance(other, Protein):
-            return NotImplemented
-        else:
-            return self.name == other.name and self.monomers == other.monomers
+    def __init__(self, name, sequence=''):
+        super().__init__(name, sequence, self.amino_acids)
 
 
-class MRNA(Molecule, Polymer):
+class MRNA(Polymer):
     nuc_acids = {'A': 1.0, 'U': 2.2, 'G': 2.1, 'C': 1.3}
 
-    def __init__(self, name, monomers=''):
-        Molecule.__init__(self, name)
-        Polymer.__init__(self, self.nuc_acids, monomers)
-
-    def __eq__(self, other):
-        if not isinstance(other, MRNA):
-            return NotImplemented
-        else:
-            return self.name == other.name and self.monomers == other.monomers
+    def __init__(self, name, sequence=''):
+        super().__init__(name, sequence, self.nuc_acids)
 
 
 class MoleculeCollection:
@@ -71,36 +61,37 @@ class MoleculeCollection:
         self.molecule_type = molecule_type
         self.molecules = None
 
-    def add(self, molecule, number):
-        """Adds number molecules to the collection."""
+    def add(self, molecule):
         if not isinstance(molecule, self.molecule_type):
             raise ValueError('Expected object of type {}, received of type {}'
                              .format(self.molecule_type, type(molecule)))
 
-    def pop(self, name, number):
-        """Removes number molecules named name from the collection. When number equals
-        zero, all are removed."""
+    def take(self, name, number):
         pass
 
     def count(self, name):
-        """Counts molecules named name."""
         pass
+
+    def populate(self, name, number):
+        for _ in range(number):
+            self.add(self.molecule_type(name))
 
 
 class PopulationCollection(MoleculeCollection):
     def __init__(self, molecule_type):
         super().__init__(molecule_type)
-        self.molecules = defaultdict(int)
+        self.molecules = dict()
 
-    def add(self, molecule, number=1):
-        super().add(molecule, number)
-        self.molecules[molecule.name] += number
-
-    def pop(self, name, number=1):
-        if number == 0:
-            self.molecules[name] = 0
+    def add(self, molecule):
+        super().add(molecule)
+        if molecule.name in self.molecules.keys():
+            self.molecules[molecule.name] += 1
         else:
-            self.molecules[name] -= number
+            self.molecules[molecule.name] = 1
+
+    def take(self, name, number=1):
+        assert self.molecules[name] >= number
+        self.molecules[name] -= number
 
     def count(self, name):
         return self.molecules[name]
@@ -109,33 +100,32 @@ class PopulationCollection(MoleculeCollection):
 class ParticleCollection(MoleculeCollection):
     def __init__(self, molecule_type):
         super().__init__(molecule_type)
-        self.molecules = defaultdict(list)
+        self.molecules = dict()
 
-    def add(self, molecule, number=1):
-        super().add(molecule, number)
-        for _ in range(number):
+    def add(self, molecule):
+        super().add(molecule)
+        if molecule.name in self.molecules.keys():
             self.molecules[molecule.name].append(copy(molecule))
+        else:
+            self.molecules[molecule.name] = [copy(molecule)]
 
-    def pop(self, name, number=1, matcher=lambda x: True):
-        """To select for molecules with certain properties, such as polymer length,
-        provide a matcher function that evaluates to True for matching molecules."""
+    def take(self, name, number=1):
         result = []
-        non_matching = []
-        finished = False
-
-        while self.molecules[name] and not finished:
+        while self.molecules[name]:
             molecule = self.molecules[name].pop()
-            if matcher(molecule):
-                result.append(molecule)
-            else:
-                non_matching.append(molecule)
+            result.append(molecule)
+            if number == len(result):
+                break
 
-            finished = False if number == 0 else len(result) == number
-
-        assert number == 0 or len(result) == number
-        self.molecules[name] += non_matching
+        assert number == 0 or number == len(result)
 
         return result
 
-    def count(self, name, matcher=lambda x: True):
-        return len([x for x in self.molecules[name] if matcher(x)])
+    def count(self, name):
+        return len([x for x in self.molecules[name]])
+
+    def get_molecules(self, name=None):
+        if not name:
+            return [molecule for molecules in self.molecules.values() for molecule in molecules]
+        else:
+            return self.molecules[name]
